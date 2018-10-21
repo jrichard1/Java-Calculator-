7e96548f7d1218a810b9173f7a99bdd5c4d6d71c
Jason Richard <jlirchard1978@gmail.com>
2018-10-21 18:46:35 -0400

    First Commit Uploading Files

---
+++ Giraffe/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+	public static void main(String [] args) {
+		Scanner keyboardInput = new Scanner(System.in);
+		System.out.print("Enter in first Number: ");
+		double num1 = keyboardInput.nextDouble();
+		System.out.print("Enter in second Number: ");
+		double num2 = keyboardInput.nextDouble();
+		System.out.println(num1 + num2);
+		
+		
+		
+  }
+}
